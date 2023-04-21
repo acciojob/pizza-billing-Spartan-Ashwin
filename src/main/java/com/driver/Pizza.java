fc7b508eff2cbb6e319509c283299c10b8c7aeb0
Ashwin Shelke <ashwinkumarshelke9@gmail.com>
2023-04-21 23:07:53 +0530

    Pizza Bill Code

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,86 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
+
+    public boolean isVeg;
+    public int basePrice;
+
+    public  int extraCheese;
+
+    public  boolean isCheeseAdded;
+
+    public  int extraTopping;
+    public  boolean isExtraToppingAdded;
+
+    public  int paperBag;
+
+    public  boolean isPaperBagTaken;
+
+    int total;
+    String bill;
 
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
+
         // your code goes here
+        this.isVeg=isVeg;
+
+        if(isVeg){
+
+            basePrice=300;
+            extraTopping=70;
+        }else{
+            basePrice=400;
+            extraTopping=120;
+        }
+        extraCheese=80;
+        paperBag=20;
+        total=basePrice;
+        bill="Base Price Of The Pizza: "+basePrice+"\n";
     }
 
     public int getPrice(){
-        return this.price;
+        return total;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(isCheeseAdded==false){
+            total=total+extraCheese;
+            isCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isExtraToppingAdded==false){
+            total+=extraTopping;
+            isExtraToppingAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(isPaperBagTaken==false){
+            total+=paperBag;
+            isPaperBagTaken=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(isCheeseAdded){
+            bill=bill+"Extra Cheese Added: "+extraCheese+"\n";
+        }
+
+        if(isExtraToppingAdded){
+            bill=bill+"Extra Toppings Added: "+extraTopping+"\n";
+        }
+
+        if(isPaperBagTaken){
+            bill=bill+"Paperbag Added: "+basePrice+"\n";
+        }
+
+        bill=bill+"Total Price: "+total+"\n";
+        return bill;
     }
 }
